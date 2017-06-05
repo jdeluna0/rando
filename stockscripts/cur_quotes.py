dec0502cd997c8753c45f7833a953ecd969cbb59
James De Luna <jdeluna0@utexas.edu>
2017-06-05 14:13:23 -0500

    changed to a function

--- stockscripts/cur_quotes.py
+++ stockscripts/cur_quotes.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python
 
-from googlefinance import getQuotes
-import json
+def quotes():
+    from googlefinance import getQuotes
 
+    stocklist = raw_input('Enter stocks for current quote: ').split()
 
-print json.dumps(getQuotes(['NEO', 'SLW']), indent=2)
+    print
+    print "{0}   {1}   {2}".format('Stock Symbol', 'Last Trade Price', 'Last Trade Time')
+    for stock in stocklist:
+        try:
+            quote = getQuotes([stock])[0]
+        except:
+            print
+            print "{0} not found.".format(stock)
+            print
+            exit()
+        print "{0:14} {1:>16} {2:>17}".format(quote['StockSymbol'],quote['LastTradePrice'],quote['LastTradeTime'])
+    print
