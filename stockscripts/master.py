d431f1ba57a859071f5360e188aad20ff22b3b5e
James De Luna <jdeluna0@utexas.edu>
2017-07-12 17:08:48 -0500

    .

--- stockscripts/master.py
+++ stockscripts/master.py
@@ -3,9 +3,11 @@
 import datetime
 import mathmulti_functions as m
 import google_intraday as g
+from MACross import Strategy, MovingAverageCrossStrategy, Portfolio, MarketOnClosePortfolio
 from pcandle import pandas_candlestick_ohlc
 from cur_quotes import quotes
 from stock_data import stocklist,single
+from pandas_datareader import data, wb
 
 def init_dates():
     choose_date = raw_input('Date to begin data collection in the format MM DD YYYY: ')
@@ -64,8 +66,46 @@ while True:
         end = tup[1]
         stocks = stocklist(start,end)
         m.prices(stocks)
-
-
+    elif opt == 'CO':
+        tup = init_dates()
+        start = tup[0]
+        end = tup[1]
+        symbol = 'AMZN'
+        bars = data.DataReader(symbol, "google", start, end)
+        # Create a Moving Average Cross Strategy instance 
+        # with short and long moving average windows
+        mac = MovingAverageCrossStrategy(symbol, bars, short_window=40, long_window=100)
+        signals = mac.generate_signals()
+        # Create a portfolio of AMZN, with $100,000 initial capital
+        portfolio = MarketOnClosePortfolio(symbol, bars, signals, initial_capital=100000.0)
+        returns = portfolio.backtest_portfolio()
+        # Plot two charts to assess trades and equity curve
+        fig = plt.figure()
+        fig.patch.set_facecolor('white')     # Set the outer colour to white
+        ax1 = fig.add_subplot(211,  ylabel='Price in $')
+        # Plot the AMZN closing price overlaid with the moving averages
+        bars['Close'].plot(ax=ax1, color='r', lw=2.)
+        signals[['short_mavg', 'long_mavg']].plot(ax=ax1, lw=2.)
+        # Plot the "buy" trades against AMZN
+        ax1.plot(signals.ix[signals.positions == 1.0].index, 
+                 signals.short_mavg[signals.positions == 1.0],
+                 '^', markersize=10, color='m')
+        # Plot the "sell" trades against AMZN
+        ax1.plot(signals.ix[signals.positions == -1.0].index, 
+                 signals.short_mavg[signals.positions == -1.0],
+                 'v', markersize=10, color='k')
+        # Plot the equity curve in dollars
+        ax2 = fig.add_subplot(212, ylabel='Portfolio value in $')
+        returns['total'].plot(ax=ax2, lw=2.)
+        # Plot the "buy" and "sell" trades against the equity curve
+        ax2.plot(returns.ix[signals.positions == 1.0].index, 
+                 returns.total[signals.positions == 1.0],
+                 '^', markersize=10, color='m')
+        ax2.plot(returns.ix[signals.positions == -1.0].index, 
+                 returns.total[signals.positions == -1.0],
+                 'v', markersize=10, color='k')
+        # Plot the figure
+        fig.show()
     elif opt == 'quote':
         quotes()
     else:
--- stockscripts/volit_stock_list.py
+++ stockscripts/volit_stock_list.py
@@ -1,31 +1,33 @@
 import requests
 from bs4 import BeautifulSoup
 import google_intraday as g
-import datetime
+from datetime import date,timedelta
 import pandas as pd
 
-#https://www.benzinga.com/news/17/06/9669712/22-stocks-moving-in-thursdays-pre-market-session - 6/29
-#https://www.benzinga.com/news/17/06/9675719/22-stocks-moving-in-fridays-pre-market-session - 6/30
-#https://www.benzinga.com/news/17/07/9681342/22-stocks-moving-in-mondays-pre-market-session - 7/3
+#url_string = 'https://www.benzinga.com/news/17/06/9669712/22-stocks-moving-in-thursdays-pre-market-session' # 6/29
+#url_string = 'https://www.benzinga.com/news/17/06/9675719/22-stocks-moving-in-fridays-pre-market-session' # 6/30
+url_string = 'https://www.benzinga.com/news/17/07/9681342/22-stocks-moving-in-mondays-pre-market-session' # 7/3
 
 
-
+date_posted = date(2017, 6, 29)
+todate = date.today()
+daygenerator = (date_posted + timedelta(x + 1) for x in xrange((todate - date_posted).days))
+diff_days = sum(1 for day in daygenerator if day.weekday() < 5) 
 
 def get_tickers(parsed_html):
 	a = []
-	for div in soup.findAll('a', attrs={'class':'ticker'}):
+	for div in parsed_html.findAll('a', attrs={'class':'ticker'}):
 		a.append(div.contents[0])
 	return a
 
 
 
 def get_intraday_data(ticker):
-	q = g.GoogleIntradayQuote(ticker,300,3)
+	q = g.GoogleIntradayQuote(ticker,300,diff_days) # Last value determines how many days to go back
 	count = 0
 	for entry in q.date:
-		if entry == datetime.date(2017, 6, 29):
+		if entry == date_posted:
 			count += 1
-
 	dict_list = []
 	#dict_list = [{'ticker':ticker,'date':str(q.date[0]),'time':str(q.time[0]),'open':q.open_[0],'high':q.high[0],'low':q.low[0],'close':q.close[0],'volume':q.volume[0]}]
 	for i in range(0, count):
@@ -34,11 +36,47 @@ def get_intraday_data(ticker):
 	return df
 
 
-r = requests.get('https://www.benzinga.com/news/17/06/9669712/22-stocks-moving-in-thursdays-pre-market-session') 
+r = requests.get(url_string) 
 soup = BeautifulSoup(r.content, 'html.parser')
 ticker_list = get_tickers(soup)
 
-dataframe_list = []
+dfd = {}
 for symbol in ticker_list:
-	dataframe_list.append(get_intraday_data(symbol))
-print dataframe_list
+	dfd[symbol] = get_intraday_data(symbol)
+print dfd['PIXY']
+exit()
+#print dataframe_list.keys()
+max_diff_total = 0
+min_diff_total = 0
+print 'OPEN\tMAX Diff\tMIN Diff'
+for ticker in dfd.keys():
+	max_times = []
+	min_times = []
+	df = dfd[ticker]
+	dayop = df.iloc[0]['open']
+	peak_values = df.loc[df['time'] == '10:15:00']
+	max_values = df.loc[df['high'] == df['high'].max()]
+	min_values = df.loc[df['low'] == df['low'].min()]
+	for index in max_values.index.values:
+		max_times.append(max_values['high'][index])
+	for index in min_values.index.values:
+		min_times.append(min_values['high'][index])
+	#print '{0}\t{1}\t{2}'.format(dayop,max_times,min_times)
+	max_diff = max_times[0] - dayop
+	min_diff = dayop - min_times[0]
+	print '{0}\t{1}\t{2}'.format(ticker,max_diff,min_diff)
+	max_diff_total = max_diff_total + max_diff
+	min_diff_total = min_diff_total + min_diff
+print 'Total\t{0}\t{1}'.format(max_diff_total,min_diff_total)
+#max_times_unique = list(set(max_times))
+#min_times_unique = list(set(min_times))
+#print 'max times'
+#for i in max_times_unique:
+#	print '{0}\t{1}'.format(i,max_times.count(i))
+#print 'min times'
+#for i in min_times_unique:
+	#print '{0}\t{1}'.format(i,min_times.count(i))
+	#max_times.append(max_values['time'][index] for index in max_values.index.values)
+	#value_list.append({'ticker':ticker,'open':open_values['open'],'max':[max_values['high'],max_values['time']]})
+	#print '{0}\nopen:{1}\nmax:{2}\t{3}\n\n'.format(ticker,dayop,list(max_values['high'][index] for index in max_values.index.values),list(max_values['time'][index] for index in max_values.index.values))
+	#	
